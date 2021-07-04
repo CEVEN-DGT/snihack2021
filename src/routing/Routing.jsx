5f9ac9ea1375ff620e503ea57897da486754170c
ShamGir <sham@troontechnologies.com>
2021-07-04 20:02:55 +0500

    Chart implemented

--- src/routing/Routing.jsx
+++ src/routing/Routing.jsx
@@ -7,36 +7,36 @@ import SignIn from "../screens/SignIn";
 import SignUp from "../screens/SignUp";
 import PrivateRoute from "./PrivateRoute";
 
-
 class Routing extends React.Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/" render={() => <SignIn {...this.props} />} />
+ render() {
+  return (
+   <Router>
+    <Switch>
+     <Route exact path="/" render={() => <SignIn {...this.props} />} />
 
-          <Route exact path="/signup" render={() => <SignUp {...this.props} />} />
+     <Route exact path="/signup" render={() => <SignUp {...this.props} />} />
 
-          <PrivateRoute
-            exact
-            path="/home"
-            render={() => <Home {...this.props} />}
-          />
-          <PrivateRoute
-            exact
-            path="/checkin"
-            render={() => <CheckIn {...this.props} />}
-          />
-          <PrivateRoute
-            exact
-            path="/park-info"
-            render={() => <ParkGraph {...this.props} />}
-          />
+     <Route
+      exact
+      path="/page-graph"
+      render={() => <ParkGraph {...this.props} />}
+     />
 
-        </Switch>
-      </Router>
-    );
-  }
+     <PrivateRoute exact path="/home" render={() => <Home {...this.props} />} />
+     <PrivateRoute
+      exact
+      path="/checkin"
+      render={() => <CheckIn {...this.props} />}
+     />
+     <PrivateRoute
+      exact
+      path="/park-info"
+      render={() => <ParkGraph {...this.props} />}
+     />
+    </Switch>
+   </Router>
+  );
+ }
 }
 
 export default Routing;
--- src/screens/ParkGraph.jsx
+++ src/screens/ParkGraph.jsx
@@ -1,12 +1,141 @@
-import React, { Component } from "react";
-import ExampleChart from '../assets/images/chart-examp.png'
+import React from "react";
+// import { Chart } from "react-charts";
+import { ResponsiveLine } from "@nivo/line";
+// import data from './data.json'
 
-export default class ParkGraph extends Component {
-  render() {
-    return (
-      <div style={{ width: "30%", margin: "auto", marginTop: "10%" }}>
-        <img src={ExampleChart} />
-      </div>
-    );
-  }
+export default function ParkGraph() {
+ //  const data = React.useMemo(
+ //   () => [
+ //    {
+ //     label: "Series 1",
+ //     data: [
+ //      [0, 1],
+ //      [1, 2],
+ //      [2, 4],
+ //      [3, 2],
+ //      [4, 7],
+ //     ],
+ //    },
+ //    {
+ //     label: "Series 2",
+ //     data: [
+ //      [0, 3],
+ //      [1, 1],
+ //      [2, 5],
+ //      [3, 6],
+ //      [4, 4],
+ //     ],
+ //    },
+ //   ],
+ //   []
+ //  );
+ //  const axes = React.useMemo(
+ //   () => [
+ //    { primary: true, type: "linear", position: "bottom" },
+ //    { type: "linear", position: "left", stacked: true },
+ //   ],
+ //   []
+ //  );
+ //  const series = React.useMemo(() => ({ type: "area" }), []);
+
+ const data = [
+  {
+   id: "japan",
+   color: "red",
+   data: [
+    {
+     x: "0",
+     y: 151,
+    },
+    {
+     x: "30",
+     y: 106,
+    },
+    {
+     x: "60",
+     y: 44,
+    },
+    {
+     x: "90",
+     y: 264,
+    },
+    {
+     x: "120",
+     y: 275,
+    },
+    {
+     x: "150",
+     y: 34,
+    },
+    {
+     x: "180",
+     y: 211,
+    },
+    {
+     x: "210",
+     y: 288,
+    },
+   ],
+  },
+ ];
+ return (
+  <div className="signup-main-container graph-main-container">
+   <div className="graph-chart-container">
+    {/* <Chart data={data} axes={axes} series={series}  /> */}
+    <ResponsiveLine
+     data={data}
+     margin={{
+      top: 50,
+      right: 50,
+      bottom: 50,
+      left: 50,
+     }}
+     xScale={{
+      type: "point",
+     }}
+     yScale={{
+      type: "linear",
+      stacked: true,
+      min: "auto",
+      max: "auto",
+     }}
+     minY="auto"
+     maxY="auto"
+     stacked={true}
+     curve="cardinal"
+     axisBottom={{
+      orient: "bottom",
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      legend: "transportation",
+      legendOffset: 36,
+      legendPosition: "center",
+     }}
+     axisLeft={{
+      orient: "left",
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      legend: "count",
+      legendOffset: -40,
+      legendPosition: "center",
+     }}
+     dotSize={10}
+     dotColor="inherit:darker(0.3)"
+     dotBorderWidth={2}
+     dotBorderColor="#ffffff"
+     enableDotLabel={true}
+     dotLabel="y"
+     dotLabelYOffset={-12}
+     animate={true}
+     motionStiffness={90}
+     enableArea={true}
+     areaOpacity={0.5}
+     motionDamping={15}
+    />
+   </div>
+   <div className="graph-details-container">Details</div>
+  </div>
+ );
 }
